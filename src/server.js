fc346882d362e07dd433efae8a415cd91511a0c0
Ewerton <64111135+ewertonmac@users.noreply.github.com>
2020-08-13 12:14:00 -0300

    modificacoes em 'server.js'

--- src/server.js
+++ src/server.js
@@ -2,9 +2,39 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const database = require('./database')
+
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/produtos',(req, res, next) => {
-    res.send({nome: 'Notebook', preco: 123.45})
+    res.send(database.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(database.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = database.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send (produto)
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = database.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco,
+        id: req.params.id
+    })
+    res.send (produto)
+})
+
+app.delete('/produtos/:id',(req, res, next) => {
+    const produto = database.excluirProduto(req.params.id)
+    res.send(produto)
 })
 
 app.listen(porta, () => {
